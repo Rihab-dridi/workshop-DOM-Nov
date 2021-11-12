8443476cb48b0c8189b4b010ad70f4f56dd3d264
Rihab Dridi <73877171+Rihab-dridi@users.noreply.github.com>
2021-11-12 11:17:57 +0100

    Add files via upload

---
+++ workshop dom NOV/main.js
@@ -0,0 +1,5 @@
+//  var title=document.getElementById('title').innerHTML="hello world"
+ //var title=document.getElementById('title').style.color="red"
+// var title=document.getElementsByTagName('h1')
+//  var title=document.querySelectorAll('h1')
+// console.log(title)
